cbc611bc5e6686b00c54b7988cdcfd8bffe878d6
Bookis Smuin <bookis.smuin@gmail.com>
2015-10-06 09:40:19 -0700

    add bad code

--- test.rb
+++ test.rb
@@ -3,4 +3,21 @@ class Blah
       1 + (1)
     end
 
+
+    def blahBlah()
+      (1..10).each do |n|
+        (1..10).each do |i|
+          puts i
+        end
+        puts n
+        "Cool Code " +
+        "Which is too long " +
+        "And Messy " +
+        "Cool Code " +
+        "Which is too long " +
+        "And Messy " +
+        "Cool Code " +
+        "Which is too long " +
+        "And Messy " 
+      end
 end
